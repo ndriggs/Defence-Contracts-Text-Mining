b3e41dcf416630d1591d023d32e6c4e86a447d4b
ndriggs <nathanieldriggs@gmail.com>
2020-01-31 22:43:03 -0700

    Update Defense.R

--- Defense.R
+++ Defense.R
@@ -1,8 +1,10 @@
 install.packages("R.utils")
 install.packages("rvest")
+install.packages("dplyr")
 
 library("R.utils")
 library("rvest")
+library("dplyr")
 
 #load the page we would like to mine
 url <- "https://www.defense.gov/Newsroom/Contracts/Contract/Article/2028834/"
@@ -43,12 +45,26 @@ money_pattern <- "awarded\\san?\\s.*?(?:maximum\\s)?.*?\\$\\K\\d+(?:\\,\\d{3})+"
 contract_amounts <- regmatches(contract_text, gregexpr(money_pattern, contract_text, perl = TRUE))
 contract_amounts
 
+
+
+
 company_names[lengths(company_names) == 0] <- NA_character_
-contract_amounts[lengths(contract_amounts) == 0] <- NA_character_
+contract_amounts[lengths(contract_amounts) == 0] <- NA_integer_
+
+#******haven't tried these 2 lines yet, can see if it makes a difference
+company_names <- as.character(company_names)
+contract_amounts <- as.numeric(paste(contract_amounts)) #<------ figure out how to convert "factor"
+#or list to numeric or integer
+
+
 
 #put the two lists in a data frame
+#*********change here, do new_data <- data_frame(company_names=company_names, etc) (data_frame is from dplyr)
 new_data <- cbind(company_names, contract_amounts)
 new_data <- data.frame(new_data)
+
+df <- data_frame(company_names=company_names, contract_amounts=contract_amounts)
+
 
 
 #We're here now!
@@ -68,8 +84,14 @@ new_data <- data.frame(new_data)
 #Is there a better way to do this? Add the new data to the running data frame
 #Also, how do you have a data frame that you always have running each time you rerun your code, do you just have to initialize it the first
 #time?
-running_tally <- data.frame(company_names = NA, total_contract_amount = NA)
-new_data <- merge(x=new_data, y=new_data, by = "company_names", all.x=TRUE)
+running_tally <- data_frame(company_names = NA, total_contract_amount = NA)
+
+running_tally$company_names <- as.character(running_tally$company_names)
+running_tally$total_contract_amount <- as.numeric(running_tally$total_contract_amount)
+
+
+running_tally <- merge(x=running_tally, y=new_data, by = "company_names", all.x=TRUE, all.y = TRUE)
+
 running_tally$total_contract_amount <- running_tally$total_contract_amount + running_tally$contract_amounts
 running_tally$contract_amounts <- NULL
 
@@ -78,4 +100,3 @@ running_tally$contract_amounts <- NULL
 if(company_names == df$Company){
   df$TotalContractsValue += contract_amounts
 }
-

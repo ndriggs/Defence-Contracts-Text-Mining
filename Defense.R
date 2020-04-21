3b53557dcb5126db50575048471c05ca0e338891
ndriggs <nathanieldriggs@gmail.com>
2020-04-21 15:46:43 -0600

    Done!!
    Now just need to collect the data and write the code to make the plots

--- Defense.R
+++ Defense.R
@@ -1,10 +1,14 @@
 install.packages("R.utils")
 install.packages("rvest")
-install.packages("dplyr")
+install.packages("tidyverse")
 
 library("R.utils")
 library("rvest")
-library("dplyr")
+library("tidyverse")
+
+#create a backup file of our current data, then load the data into our program
+file.copy("contract_data.csv", "backup.csv")
+all_data <- read.csv("contract_data.csv")
 
 #load the page we would like to mine
 url <- "https://www.defense.gov/Newsroom/Contracts/Contract/Article/2028834/"
@@ -16,21 +20,12 @@ contract_text_html <- html_nodes(webpage, "p")
 #convert it to text
 contract_text <- html_text(contract_text_html)
 
-#modifying the vector- look for (\r\n)+ and then seperate them into two different elements
-#or can use look behind for the newline
-#to seperate can use the contract_text <- insert(contract_text, ats = pos, value) from R.utils package
-
-#NEXT STEPS:
-
-#1. get the data frame stuff up and running
-
-#make sure each paragraph has its own element in the vector
+#make sure each paragraph has its own seperate spot in the vector
 second_paragraph_pattern <- "(?<=\\r\\n).{90,}"
 second_paragraphs <- regmatches(contract_text, gregexpr(second_paragraph_pattern, contract_text, perl = TRUE))
 contract_text <- gsub(second_paragraph_pattern, "", contract_text, perl = TRUE)
 
-
-#find where those second paragraphs are
+#find where those double paragraphs are
 indicies <- which(sapply(second_paragraphs, length) > 0) + 1
 second_paragraphs <- second_paragraphs[lengths(second_paragraphs) > 0]
 contract_text <- insert(contract_text, ats = indicies, values=second_paragraphs)
@@ -45,47 +40,21 @@ money_pattern <- "awarded\\san?\\s.*?(?:maximum\\s)?.*?\\$\\K\\d+(?:\\,\\d{3})+"
 contract_amounts <- regmatches(contract_text, gregexpr(money_pattern, contract_text, perl = TRUE))
 contract_amounts
 
+#delete rows where there's not data for both
+contract_amounts <- as.numeric(gsub(",", "", as.character(contract_amounts)))
+contract_amounts[(lengths(company_names) == 0)] <- NA
+company_names[is.na(contract_amounts)] <- NULL
+company_names <- unlist(company_names)
+contract_amounts <- contract_amounts[!is.na(contract_amounts)]
 
+#create a date column
+date <- rep(Sys.Date(), times=length(company_names))
 
+#put the newly extracted data all together in a data frame
+new_data <- data.frame(date, company_names, contract_amounts)
+names(new_data) <- c("Date", "Company Name", "Contract Amount")
 
-company_names[lengths(company_names) == 0] <- ""
-contract_amounts[lengths(contract_amounts) == 0] <- 0
+#merge the old and new data
+all_data <- rbind(all_data, new_data)
 
-#******haven't tried these 2 lines yet, can see if it makes a difference
-company_names <- as.character(company_names)
-contract_amounts <- as.numeric(contract_amounts) #<------ figure out how to convert "factor"
-#or list to numeric or integer
-
-
-#put the two lists in a data frame
-#*********change here, do new_data <- data_frame(company_names=company_names, etc) (data_frame is from dplyr)
-new_data <- cbind(company_names, contract_amounts)
-new_data <- data.frame(new_data)
-
-#df <- data.frame("company_names"=company_names, "contract_amounts"=contract_amounts)
-#df <- data.frame(company_names, contract_amounts)
-
-duplicated(new_data$company_names)
-
-
-#Is there a better way to do this? Add the new data to the running data frame
-#Also, how do you have a data frame that you always have running each time you rerun your code, do you just have to initialize it the first
-#time?
-running_tally <- data.frame("company_names" = c("Lockheed Martin Corp.", "KPMG LLP", "Other Company"), "total_contract_amount" = c(100, 0, 1))
-
-running_tally$company_names <- as.character(running_tally$company_names)
-running_tally$total_contract_amount <- as.numeric(running_tally$total_contract_amount)
-
-
-
-
-running_tally <- merge(x=running_tally, y=new_data, by = "company_names", all.x=TRUE, all.y = TRUE)
-
-running_tally$total_contract_amount <- running_tally$total_contract_amount + running_tally$contract_amounts
-running_tally$contract_amounts <- NULL
-
-
-
-if(company_names == df$Company){
-  df$TotalContractsValue += contract_amounts
-}
+write.csv(all_data, "contract_data.csv")
